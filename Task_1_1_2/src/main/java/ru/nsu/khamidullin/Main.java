ee405cc8eea810c2ccec6558b26772683da24c3e
Rustam-Khamidullin <rstm.khamidullin@gmail.com>
2023-09-27 21:54:28 +0700

    Task_1_1_2 (version 2)

--- Task_1_1_2/src/main/java/ru/nsu/khamidullin/Main.java
+++ Task_1_1_2/src/main/java/ru/nsu/khamidullin/Main.java
@@ -3,10 +3,11 @@ package ru.nsu.khamidullin;
 
 public class Main {
     public static void main(String[] args) {
-        var pol = new Polynomial(new int[]{0,-1,-1,-1,-1});
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {3, 2, 8});
 
-        System.out.println(pol.toString());
-
-
+        System.out.println(p1.plus(p2.differentiate(1)).toString());
+        System.out.println(p1.times(p2).evaluate(2));
+        System.out.println(p1.times(p2));
     }
 }
--- Task_1_1_2/src/main/java/ru/nsu/khamidullin/Polynomial.java
+++ Task_1_1_2/src/main/java/ru/nsu/khamidullin/Polynomial.java
@@ -1,64 +1,187 @@
 package ru.nsu.khamidullin;
 
 public class Polynomial {
-    static int[] coefficients;
-
-    public Polynomial(int[] coefficients) {
-        if (coefficients.length == 0) {
-            this.coefficients = new int[0];
-        } else {
-            this.coefficients = coefficients.clone();
+    private String doubleToString(double x) {
+        if (x % 1 == 0) {
+            return String.valueOf((int) x);
         }
+        return String.valueOf(x);
     }
 
-    public static int[] getCof() {
-        return coefficients;
+    public void delZero() {
+        int len = coefficients.length;
+        while (len > 1 && coefficients[len - 1] == 0) {
+            len -= 1;
+        }
+        double[] newCoef = new double[len];
+        for (int i = 0; i < len; i++) {
+            newCoef[i] = coefficients[i];
+        }
+        coefficients = newCoef;
+    }
+
+
+    private double[] coefficients;
+
+    public Polynomial(double[] inputCoef) {
+        if (inputCoef.length == 0) {
+            coefficients = new double[]{0};
+        } else {
+            coefficients = inputCoef.clone();
+        }
+        delZero();
+    }
+
+    public Polynomial(int[] inputCoef) {
+        if (inputCoef.length == 0) {
+            coefficients = new double[]{0};
+        } else {
+            coefficients = new double[inputCoef.length];
+            for (int i = 0; i < inputCoef.length; i++) {
+                coefficients[i] = (double) inputCoef[i];
+            }
+        }
+        delZero();
+    }
+
+
+    public double[] getCoefficients() {
+        return coefficients.clone();
     }
 
     public Polynomial plus(Polynomial polynomial) {
-        int[] cof = polynomial.getCof().clone();
-        if (cof.length > coefficients.length) {
-            for (int i = 0; i < coefficients.length; i++) {
-                cof[i] += coefficients[i];
+        double[] coef = polynomial.getCoefficients();
+
+        double [] res = new double[Math.max(coef.length, coefficients.length)];
+        for (int i = 0; i < res.length; i++) {
+            if (i < coef.length) {
+                res[i] += coef[i];
             }
-            coefficients = cof;
-        } else {
-            for (int i = 0; i < cof.length; i++) {
-                coefficients[i] += cof[i];
+            if (i < coefficients.length) {
+                res[i] += coefficients[i];
             }
         }
-        return this;
+        var resPol = new Polynomial(res);
+        resPol.delZero();
+        return resPol;
+    }
+
+    public Polynomial minus(Polynomial polynomial) {
+        var negCoef = polynomial.getCoefficients();
+        for (int i = 0; i < negCoef.length; i++) {
+            negCoef[i] = -negCoef[i];
+        }
+
+        return plus(new Polynomial(negCoef));
+    }
+
+    public Polynomial times(Polynomial polynomial) {
+        double[] coef = polynomial.getCoefficients();
+        double[] newCoef = new double[coefficients.length + coef.length];
+
+        for (int i = 0; i < coef.length; i++) {
+            for (int j = 0; j < coefficients.length; j++) {
+                newCoef[i + j] += coefficients[j] * coef[i];
+            }
+        }
+        return new Polynomial(newCoef);
     }
 
 
+    public double evaluate(double x) {
+        double res = 0;
+        double X = 1;
+        for (double coefficient : coefficients) {
+            res += coefficient * X;
+            X *= x;
+        }
+        return res;
+    }
+
+
+    public Polynomial differentiate(int n) {
+        double [] newCoef = new double[coefficients.length - n];
+
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        for (int i = 0; i < newCoef.length; i++) {
+            newCoef[i] = coefficients[i + n] * fact;
+            fact = fact * (i + n + 1) / (i + 1);
+        }
+        return new Polynomial(newCoef);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Polynomial polynomial = (Polynomial) obj;
+
+        double [] coef = polynomial.getCoefficients();
+
+        if (coefficients.length != coef.length) {
+            return false;
+        }
+        for (int i = 0; i < coefficients.length; i++) {
+            if (coefficients[i] != coef[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
 
-        for (int i = coefficients.length - 1; i > 0; i--) {
+        int len = coefficients.length;
+
+        if (len > 2) {
+            if (coefficients[len - 1] < 0) {
+                str.append("- ");
+            }
+            str.append(doubleToString(Math.abs(coefficients[len - 1])));
+            str.append("x^" + (len - 1));
+        }
+
+        for (int i = coefficients.length - 2; i > 0; i--) {
             if (coefficients[i] != 0) {
                 if (coefficients[i] > 0) {
                     str.append(" + ");
+                    if (coefficients[i] != 1) {
+                        str.append(doubleToString(coefficients[i]));
+                    }
                 } else if (coefficients[i] < 0) {
                     str.append(" - ");
+                    if (coefficients[i] != -1) {
+                        str.append(doubleToString(-coefficients[i]));
+                    }
                 }
-                if (coefficients[i] != 1 && coefficients[i] != -1) {
-                    str.append(coefficients[i]);
+                if (i > 1) {
+                    str.append("x^").append(i);
+                }  else {
+                    str.append("x");
                 }
-
-                str.append("x^").append(i);
             }
         }
 
-        if (coefficients.length > 1) {
-            if (coefficients[0] > 0) {
-                str.append(" + ").append(coefficients[0]);
-            } else if (coefficients[0] < 0) {
-                str.append(" - ").append(-coefficients[0]);
-            }
+        if (coefficients[0] < 0) {
+            str.append(" - ").append(doubleToString(-coefficients[0]));
         }
-
+        else if (len > 1 && coefficients[0] != 0) {
+            str.append(" + ").append(doubleToString(coefficients[0]));
+        } else if (len == 1) {
+            str.append(doubleToString(coefficients[0]));
+        }
         return str.toString();
     }
-
-
 }
