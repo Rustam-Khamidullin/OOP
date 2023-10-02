513d683aa889d7f40625fec9553ea79a3271850a
Rustam-Khamidullin <rstm.khamidullin@gmail.com>
2023-10-02 10:05:20 +0700

    lastest

--- Task_1_1_1/src/main/java/ru/nsu/khamidullin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/khamidullin/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
 
-        heapsort(arr);
+        arr = heapsort(arr);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
