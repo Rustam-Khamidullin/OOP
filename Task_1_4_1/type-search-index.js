7d4bc5f8418e8f42e788007cbac3b0fba0a506f5
Rustam-Khamidullin <Rustam-Khamidullin@users.noreply.github.com>
2023-11-20 18:38:00 +0000

    Deploying to gh-pages from @ Rustam-Khamidullin/OOP@0ea0543e9522eefbcdbd34656d8e027f6f01d442 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.khamidullin","l":"CreditBook"},{"p":"ru.nsu.khamidullin","l":"Mark"},{"p":"ru.nsu.khamidullin","l":"Subject"}]
